9d7d7e94bf9e698fc7c577b701480387c9f0fa25
rodger2003 <48264113+dani0303@users.noreply.github.com>
2021-05-08 21:04:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, redirect, url_for
+import csv, urllib.request
+
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    results = []
+    url = 'https://s3.amazonaws.com/rawstore.datahub.io/27e05e0885fe41e0d9dfa87e87892458.csv'
+    response = urllib.request.urlopen(url)
+    lines = [l.decode('utf-8') for l in response.readlines()]
+    cr = csv.reader(lines)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    for row in cr:
+        results.append(row)
+
+    return render_template('table.html', results=results)
